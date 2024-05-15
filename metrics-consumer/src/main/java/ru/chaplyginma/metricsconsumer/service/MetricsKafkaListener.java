a102e1f40ac92f44653c5d141f30a1bad20211fe
GibbedHead <chaplygin-ma@yandex.ru>
2024-05-16 00:10:09 +0300

    Добавил транзакционность в обработку сообщений в консьюмере

--- metrics-consumer/src/main/java/ru/chaplyginma/metricsconsumer/service/MetricsKafkaListener.java
+++ metrics-consumer/src/main/java/ru/chaplyginma/metricsconsumer/service/MetricsKafkaListener.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.chaplyginma.metricsconsumer.dto.AddMetricDto;
 
 @Service
@@ -13,6 +14,7 @@ public class MetricsKafkaListener {
     private final MetricsService metricsService;
 
     @KafkaListener(id = "metricsGroup", topics = "metrics")
+    @Transactional
     public void listenMetrics(AddMetricDto addMetricDto) {
         log.info("Listened add metrics: {}", addMetricDto);
         metricsService.save(addMetricDto);
