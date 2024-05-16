10cfa1937d7527e7f1206f2c8989490834fcecbb
GibbedHead <chaplygin-ma@yandex.ru>
2024-05-16 14:50:26 +0300

    Для продюсера добавил api docs

--- metrics-producer/src/main/java/ru/chaplyginma/metricsproducer/controller/MetricsController.java
+++ metrics-producer/src/main/java/ru/chaplyginma/metricsproducer/controller/MetricsController.java
@@ -1,5 +1,11 @@
 package ru.chaplyginma.metricsproducer.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,12 +18,19 @@ import ru.chaplyginma.metricsproducer.service.MetricsService;
 @RequestMapping(path = "/metrics")
 @Slf4j
 @RequiredArgsConstructor
+@Tag(name = "Metrics", description = "Endpoint for managing metrics")
 public class MetricsController {
     private final MetricsService metricsService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Add Metrics", description = "Add new metrics data to kafka")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Metrics added successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data")
+    })
     public String postMetrics(
+            @Parameter(description = "Metrics data to add", required = true, schema = @Schema(implementation = AddMetricsDto.class))
             @Valid @RequestBody AddMetricsDto addMetricsDto
     ) {
         log.info("Received add metrics: {}", addMetricsDto);
--- metrics-producer/src/main/java/ru/chaplyginma/metricsproducer/dto/AddMetricsDto.java
+++ metrics-producer/src/main/java/ru/chaplyginma/metricsproducer/dto/AddMetricsDto.java
@@ -1,5 +1,6 @@
 package ru.chaplyginma.metricsproducer.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -12,8 +13,11 @@ import java.time.LocalDateTime;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class AddMetricsDto {
     @NotBlank
+    @Schema(description = "Name of the metric", example = "CPU Usage")
     String metricName;
+    @Schema(description = "Value of the metric", example = "0.75")
     Double value;
     @NotNull
+    @Schema(description = "Timestamp of the metric data", example = "2024-05-16T12:00:00")
     LocalDateTime timestamp;
 }
